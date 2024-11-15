39a8e1a9eeef677cb167c34d23b8944cdd001b71
yanghj313 <lawork313@gmail.com>
2024-11-15 18:07:35 +0900

    [feat] B타입 AI도서추천 AI 추천도서 완료

--- resources/ict/js/book.js
+++ resources/ict/js/book.js
@@ -31,6 +31,42 @@ $(document).ready(function () {
 	});
 });
 
+//AI 도서추천
+$(function () {
+	$('.reco_tab a').click(function () {
+		var tab_id = $(this).attr('data-tab');
+
+		$('.reco_tab a').removeClass('active');
+		$('.tab_content').removeClass('active');
+
+		$(this).addClass('active');
+		$('#' + tab_id).addClass('active');
+	});
+});
+
+$(function () {
+	// 게시판
+	$('.recom_tab a').click(function () {
+		var tab_id = $(this).attr('data-tab');
+
+		$('.recom_tab a').removeClass('active');
+		$('.tab_contentt').removeClass('active');
+
+		$(this).addClass('active');
+		$('#' + tab_id).addClass('active');
+	});
+});
+
+$(function () {
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 1,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+});
+
 //도서검색 목록
 $(function () {
 	$('.info_fold').hide();
