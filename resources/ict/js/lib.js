592428c1acb358deaeaea767c0d9da40b70c19bf
infosetGang <kangyoonkyung@infoset.co.kr>
2024-11-18 11:44:40 +0900

    [init] 라이브러리보드 작업완료

--- resources/ict/js/lib.js
+++ resources/ict/js/lib.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 		const formattedTime = now.toTimeString().slice(0, 5);
 		const formattedDate = `${now.getFullYear()}. ${(now.getMonth() + 1).toString().padStart(2, '0')}. ${now.getDate().toString().padStart(2, '0')}. ${'일월화수목금토'[now.getDay()]}요일`;
 
-		$('.kiosk_main_time_x').text(formattedTime);
-		$('.kiosk_main_date_x').text(formattedDate);
+		$('.lib_main_time').text(formattedTime);
+		$('.lib_main_date').text(formattedDate);
 	}
 
 	updateDateTime();
